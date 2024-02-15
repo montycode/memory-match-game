e5959aef255b6066572622836260c255bdb93cef
montycode <alfredo.montoya@cesunbc.edu.mx>
2024-02-15 14:50:25 -0800

    Update App.tsx
    Now implements all use cases!

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
-import "./App.css";
+import { useState } from "react";
 import MatchGame from "./components/MatchGame";
+import data from "./data/countries.json";
+import { CountryCapital } from "./types/country-capital";
+import "./App.css";
 
 function App() {
+  const [gameWon, setGameWon] = useState(false);
+  const [gameLost, setGameLost] = useState(false);
+
+  const handleGameWin = () => {
+    setGameWon(true);
+  };
+
+  const handleGameLoss = () => {
+    setGameLost(true);
+  };
   return (
     <section className="mx-auto container w-full py-36">
       <div className="flex flex-col justify-center items-center gap">
         <div className="md:text-5xl text-4xl font-black text-center text-gray-800 leading-snug lg:w-3/4">
           <h2>Country-Capital Match Game</h2>
         </div>
-        <MatchGame />
-        <div className="md:text-2xl text-md text-center text-gray-800 leading-snug lg:w-3/4">
-          <h2>
-            Lifes: <span>❤️❤️❤️</span>
-          </h2>
-        </div>
+        {/* MatchGame component with country-capital data */}
+        {!gameWon && !gameLost && (
+          <MatchGame
+            data={data as CountryCapital}
+            onGameWin={handleGameWin}
+            onGameLoss={handleGameLoss}
+          />
+        )}
+        {gameWon && <div>Winner!</div>}
+        {gameLost && <div>You lost!</div>}
       </div>
     </section>
   );

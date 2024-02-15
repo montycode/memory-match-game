8b3c8a05780bc1e17091b3962787a12d6f8c3776
montycode <alfredo.montoya@cesunbc.edu.mx>
2024-02-15 14:49:20 -0800

    Update MatchGame.tsx
    Now implements custom hook for manage our buttons & game state

--- src/components/MatchGame.tsx
+++ src/components/MatchGame.tsx
@@ -1,16 +1,38 @@
 import React from "react";
 import CountryButton from "./CountryButton";
+import useMatchGame from "../hooks/useMatchGame";
 
-const MatchGame = () => {
+interface MatchGameProps {
+  data: { [key: string]: string };
+  onGameWin: () => void;
+  onGameLoss: () => void;
+}
+
+const MatchGame: React.FC<MatchGameProps> = ({
+  data,
+  onGameWin,
+  onGameLoss,
+}) => {
+  // Custom hook to manage the game state
+  const { buttons, lives } = useMatchGame({ data, onGameWin, onGameLoss });
+
   return (
-    <div className="mx-auto my-2 px-6 flex flex-wrap justify-center">
-      <CountryButton />
-      <CountryButton />
-      <CountryButton />
-      <CountryButton />
-      <CountryButton />
-      <CountryButton />
-    </div>
+    <>
+      <div className="mx-auto my-2 px-6 flex flex-wrap justify-center">
+        {/* Render each CountryButton based on the provided button data */}
+        {buttons.map((button) => (
+          <CountryButton {...button} />
+        ))}
+      </div>
+      <div className="md:text-2xl text-md text-center text-gray-800 leading-snug lg:w-3/4">
+        <h2>
+          Lives:{" "}
+          {Array.from({ length: lives }, (_, index) => (
+            <span key={index}>❤️</span>
+          ))}
+        </h2>
+      </div>
+    </>
   );
 };
 

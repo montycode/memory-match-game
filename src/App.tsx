319cf0240cdb417822778fc5b782fd0ffdf8be75
montycode <alfredo.montoya@cesunbc.edu.mx>
2024-02-15 12:10:26 -0800

    Expected final result sample.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import MatchGame from "./components/MatchGame";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section className="mx-auto container w-full py-36">
+      <div className="flex flex-col justify-center items-center gap">
+        <div className="md:text-5xl text-4xl font-black text-center text-gray-800 leading-snug lg:w-3/4">
+          <h2>Country-Capital Match Game</h2>
+        </div>
+        <MatchGame />
+        <div className="md:text-2xl text-md text-center text-gray-800 leading-snug lg:w-3/4">
+          <h2>
+            Lifes: <span>❤️❤️❤️</span>
+          </h2>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </section>
+  );
 }
 
-export default App
+export default App;
